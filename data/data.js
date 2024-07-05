8e9a2549ec7f56bcd50f5f73a26655fe75ab65b6
bmehder <bmehder@gmail.com>
2024-07-05 10:13:30 +0200

    added new data

--- data/data.js
+++ data/data.js
@@ -1,6 +1,7 @@
 const BOOKS_DATA = [
-	{ id: '1', title: 'The Final Empire', author: 'Brandon Sanderson' },
-	{ id: '2', title: 'The Way of Kings', author: 'Brandon Sanderson' },
+	{ id: '1', title: 'Javascript: The Good Parts', author: 'Douglas Crockford' },
+	{ id: '2', title: 'F# for Fun and Profit', author: 'Scott Wlaschin' },
+	{ id: '3', title: 'Learn You a Haskell for Great Good', author: 'Miran Lapovača' },
 ]
 
 export default BOOKS_DATA
